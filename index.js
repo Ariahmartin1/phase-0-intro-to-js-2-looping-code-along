4ca4fd940e3683c4e7042f13f5ee570f0f069d9c
zachariah martin <zachariahsmartin@gmail.com>
2022-09-10 11:55:47 -0400

    commit tree 4dc9c71764aaffa9a2cbde8d81d0b7d17f909926

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+const thankYouMessages = [];
+
+function writeCards(name, event) {
+  for (let i = 0; i < name.length; i++) {
+    thankYouMessages.push (`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+  }
+  return thankYouMessages
+}
+
+function countDown () {
+    let countdown = 10;
+    while (countdown >= 0) 
+    console.log(countdown--)
+  }
